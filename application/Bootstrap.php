fef229429e15abccefbba080d8bf54004089ff5a
Bartosz Rychlicki <b@br-design.pl>
2012-02-16 00:14:30 +0100

    Moduł zrobie w branchu, nie jestem pewien jeszcze jak to ulozyc w katalogach wiec nie bede experymentowal na master This reverts commit 003d0b687c92c60901b660d4179fb9880fd1ecc4.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -38,6 +38,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	protected function _initFireBugLogger()
 	{
 		if(!$this->_isDebugEnabled('firebuglogger')) return false;
+		if(!$this->hasPluginResource) return false;
 		$loggerResource = $this->getPluginResource('log');
 		$logger = $loggerResource->getLog();
 		
@@ -57,7 +58,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$options = array(
 			'plugins' => array('Variables',
 		    'Database' => array('adapter' => $db), 
-            'File' => array('basePath' => APPLICATION_PATH),
+            //'File' => array('basePath' => '/path/to/project'),
             //'Cache' => array('backend' => $cache->getBackend()), 
             'Exception')		
 		);
