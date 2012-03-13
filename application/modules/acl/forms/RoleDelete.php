fd3773ebf440bd3ec9d93209a7baf9808a0b6d11
Bartosz Rychlicki <b@br-design.pl>
2012-03-13 18:52:54 +0100

    poprawka do formularza, znika przycisk

--- application/modules/acl/forms/RoleDelete.php
+++ application/modules/acl/forms/RoleDelete.php
@@ -9,7 +9,6 @@ class Acl_Form_RoleDelete extends EasyBib_Form {
 		$this->setMethod('post');
 		$this->setAttrib('class', 'form-horizontal');
 		$this->setAction('delete-role');
-		$cancel      = new Zend_Form_Element_Button('cancel');
 		$this->addElement('hidden', 'id', array(
 				'filters'	=>	array('Int')
 		));
@@ -19,14 +18,7 @@ class Acl_Form_RoleDelete extends EasyBib_Form {
 		
 		$submit->setLabel('Delete');
 		$this->addElement($submit);
-		$this->addElement($cancel);
 		
-		// add display group
-		$this->addDisplayGroup(
-				array('submit', 'cancel'),
-				'Role'
-		);
-		//$this->getDisplayGroup('Role')->setLegend('Delete a role');
 		
 		// set decorators
 		EasyBib_Form_Decorator::setFormDecorator($this, EasyBib_Form_Decorator::BOOTSTRAP, 'submit', 'cancel');	
