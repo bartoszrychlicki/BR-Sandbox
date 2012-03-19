31482022ed80bc5c1e607a1beb4bf897ea11bce2
Bartosz Rychlicki <b@br-design.pl>
2012-03-19 18:45:13 +0100

    zabawy z modelemi

---
+++ library/Br/Db/Table/Row/Abstract.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* 
+*/
+abstract class Br_Db_Table_Row_Abstract extends Zend_Db_Table_Row
+{
+	protected $_validators = array();
+	protected $_error;
+
+	public function getValidator($name)
+	{
+		return array_key_exists($name, $this->_validators) ? $this->_validators : array();
+	}
+	
+	/**
+	 * Overwrite the save method to add automatic validation to model.
+	 *
+	 * If validation fail we return false, and add 
+	 */
+	public function save() 
+	{
+		if(!$this->isValid()) {
+			return false;
+		}
+		return parent::save();
+	}
+	
+	/**
+	* Return validation messages for isValid function to use in View.
+	*/
+	public function getMessages()
+	{
+		
+	}
+	
+	/**
+	 * Valids the object against $_validators specified in array.
+	 *
+	 * If validation fails, method adds messages to $_error var.
+	 *
+	 * @return bool 
+	 */
+	public function isValid()
+	{
+		return true;
+	}
+}
