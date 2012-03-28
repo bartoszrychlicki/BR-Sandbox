7e53219f23b544401690029b682c6d24451f5455
Bartosz Rychlicki <me@bartoszrychlicki.com>
2012-03-28 09:08:17 +0200

    sa

---
+++ application/modules/user/models/DbTable/Row/Interface/User.php
@@ -0,0 +1,7 @@
+<?php
+interface User_Model_DbTable_Row_Interface_User {
+    public function setPassword($password);
+    public function setUsername($username);
+    public function getUsername();
+}
+?>
